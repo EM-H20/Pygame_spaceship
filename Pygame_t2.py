54ed63f446b99663b5b8bd0e40a32f7f0475864a
EuiMin Hong <ghd0106@gmail.com>
2023-03-07 15:46:21 +0900

    third commit

--- Pygame_t2.py
+++ Pygame_t2.py
@@ -15,10 +15,12 @@ pygame.display.set_caption(title)
 # 3. 게임내 필요한 설정
 clock = pygame.time.Clock()
 
+
 class obj:
     def __init__(self):
         self.x = 0
         self.y = 0
+        self.move = 0
 
     def put_img(self, address):
         if address[-3:] == "png":
@@ -34,34 +36,97 @@ class obj:
     def show(self):
         screen.blit(self.img, (self.x, self.y))
 
-ss = obj()
+
+ss = obj()  # SpaceShip
 ss.put_img("C:/Users/82108/Desktop/workspace/image/spaceship.png")
-ss.change_size(50, 80)
+ss.change_size(80, 100)
 
 ss.x = round(size[0] / 2 - ss.sx / 2)
 ss.y = size[1] - ss.sy - 15
 
+ss.move = 7
+left_go = False
+right_go = False
+space_go = False
+m_list = []
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+
 # 4. 메인 이벤트
 SB = 0
 while SB == 0:
-
-    # 4-1, FPS 설정
+     # 4-1, FPS 설정
     clock.tick(60)
 
     # 4-2, 각종 입력 감지
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             SB = 1
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_go = True
+            elif event.key == pygame.K_RIGHT:
+                right_go = True
+            elif event.type == pygame.K_SPACE:
+                space_go = True   
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left_go = False
+            elif event.key == pygame.K_RIGHT:
+                right_go = False
+            elif event.type == pygame.K_SPACE:
+                space_go = False
+
+
+        
+
 
     # 4-3, 입력, 시간에 따른 변화
+    if left_go == True:
+        ss.x -= ss.move
+        if ss.x <= 0:
+            ss.x = 0
 
-    # 4-4, 그리기
+    elif right_go == True:
+        ss.x += ss.move
+        if ss.x >= size[0] - ss.sx:
+            ss.x = size[0] - ss.sx
+    
+    if space_go == True:
+        mm = obj()  # misile
+        mm.put_img("C:/Users/82108/Desktop/workspace/image/missile.png")
+        mm.change_size(5, 15)
+
+        mm.x = round(ss.x + ss.sx / 2 - mm.sx / 2)
+        mm.y = ss.y - mm.y - 10
+        mm.move = 15
+
+        m_list.append(mm)
+
+    d_list = []
+    for i in range(len(m_list)):
+        m = m_list
+        m.y -= m.move
+
+        if m.y <= -m.sy:
+            d_list.append(i)
+    
+    for d in d_list:
+        del m_list[d]
+
+
+
+
+    # 4-4, 그리기       
     screen.fill(black)
     ss.show()
+
+    for m in m_list:
+        m.show()
 
     # 4-5, 업데이트
     pygame.display.flip()
